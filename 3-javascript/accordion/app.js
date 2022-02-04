5560b2563b2cb0beb4c13aa8687da520ca9b60be
KS <kitsidhu@gmail.com>
2022-02-04 18:42:23 -0500

    hide answer when question is clicked, if answer is displayed

--- 3-javascript/accordion/app.js
+++ 3-javascript/accordion/app.js
@@ -1,14 +1,9 @@
 const faqList = document.querySelectorAll('.question');
 
-buildPage();
-
-function buildPage() {
-  toggleAnswer();
-}
+toggleAnswer();
 
 function toggleAnswer() {
   faqList.forEach((question) => {
-    console.log(question);
     question.addEventListener('click', (event) => {
       if(event.target.classList.contains('question')) {
         let answer = event.target.parentNode.querySelector('.answer');
@@ -16,7 +11,7 @@ function toggleAnswer() {
         if(answer.style.display === '') {
           answer.style.display = 'block';
         } else {
-          answer.style.display === '';
+          answer.style.display = '';
         }
       }
     })
