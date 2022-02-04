f390990bcb0e7fbe59d582bd7baa55838b97d0f7
KS <kitsidhu@gmail.com>
2022-02-03 21:18:07 -0500

    validate email input using regex & log result to console

--- 3-javascript/form-validation/app.js
+++ 3-javascript/form-validation/app.js
@@ -26,5 +26,12 @@ function validateEmail() {
   submitButton.addEventListener('click', function(){
     let emailInput = emailField.value;
     console.log(emailInput);
+    let expression = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let isValid = expression.test(String(emailInput).toLowerCase());
+    if(!isValid){
+      console.log('email not valid');
+    } else {
+      console.log('good');
+    }
   });
 }
