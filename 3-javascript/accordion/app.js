ba2d8f0d8319fbfbf58a41b08e11a2d77188495a
KS <kitsidhu@gmail.com>
2022-02-04 18:21:27 -0500

    grab question, add event listener to log answer in console upon click

--- 3-javascript/accordion/app.js
+++ 3-javascript/accordion/app.js
@@ -1,1 +1,20 @@
-console.log('whoopie!');
+const faqList = document.querySelectorAll('.question');
+
+buildPage();
+
+function buildPage() {
+  showAnswer();
+  // hideAnswer();
+}
+
+function showAnswer() {
+  faqList.forEach(function(question) {
+    console.log(question);
+    question.addEventListener('click', (event) => {
+      if(event.target.classList.contains('question')) {
+        let answer = event.target.parentNode.querySelector('.answer');
+        console.log(answer);
+      }
+    })
+  });
+}
