1f1249cc838519717b4dc73c8b800bcca39ba3ba
KS <kitsidhu@gmail.com>
2022-02-03 19:38:44 -0500

    grab input fields

--- 3-javascript/form-validation/app.js
+++ 3-javascript/form-validation/app.js
@@ -1,1 +1,22 @@
-console.log("whoopie!");
+const app = document.querySelector('#secondary-flex');
+console.log(app);
+
+buildPage();
+
+function buildPage() {
+  emptyFormFieldError();
+}
+
+function emptyFormFieldError() {
+  const inputFields = document.querySelectorAll('form input');
+  console.log(inputFields);
+  inputFields.forEach(function(field){
+    console.log(field);
+    field.addEventListener('submit', function(e){
+      // if(field.value ===''){
+      //   let fieldName = field.name;
+      //   console.log(fieldName);
+      // }
+    });
+  });
+}
