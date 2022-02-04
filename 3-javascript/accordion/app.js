be6305a9e4d64ef30d333ea19d3ec9042f9fa0b7
KS <kitsidhu@gmail.com>
2022-02-04 18:37:25 -0500

    change showAnswer function name to toggleAnswer & display answer in browser upon click

--- 3-javascript/accordion/app.js
+++ 3-javascript/accordion/app.js
@@ -3,17 +3,21 @@ const faqList = document.querySelectorAll('.question');
 buildPage();
 
 function buildPage() {
-  showAnswer();
-  // hideAnswer();
+  toggleAnswer();
 }
 
-function showAnswer() {
-  faqList.forEach(function(question) {
+function toggleAnswer() {
+  faqList.forEach((question) => {
     console.log(question);
     question.addEventListener('click', (event) => {
       if(event.target.classList.contains('question')) {
         let answer = event.target.parentNode.querySelector('.answer');
-        console.log(answer);
+        console.log(answer.style.display);
+        if(answer.style.display === '') {
+          answer.style.display = 'block';
+        } else {
+          answer.style.display === '';
+        }
       }
     })
   });
