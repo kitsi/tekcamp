a5640667425f3f9df479f2135c0860d72962d124
KS <kitsidhu@gmail.com>
2022-02-04 20:40:23 -0500

    add form constant & reformat anonymous fxns in event listeners as arrow fxns

--- 3-javascript/form-validation/app.js
+++ 3-javascript/form-validation/app.js
@@ -1,4 +1,5 @@
-const inputFields = document.querySelectorAll('form input'),
+const form = document.querySelector('form');
+      inputFields = document.querySelectorAll('form input'),
       emailField = document.querySelector('form input#email'),
       submitButton = document.querySelector('#form-submit');
 
@@ -11,21 +12,23 @@ function buildPage() {
 
 function displayEmptyInputFieldError() {
   inputFields.forEach(function(field){
-    submitButton.addEventListener('click', function(){
+    submitButton.addEventListener('click', () => {
       if(field.value ===''){
         let errorIcon = field.previousElementSibling;
         errorIcon.style.visibility = 'visible';
         let errorMessage = field.nextElementSibling;
         errorMessage.style.visibility = 'visible';
+        errorMessage.style.fontStyle = 'italic';
         field.classList.add('error'); //changes border to red
         field.placeholder = '';
       }
     });
   });
+
 }
 
 function validateEmail() {
-  submitButton.addEventListener('click', function(){
+  submitButton.addEventListener('click', () => {
     let emailInput = emailField.value;
     let expression = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     let isValid = expression.test(String(emailInput).toLowerCase());
