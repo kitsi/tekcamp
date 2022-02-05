aa100dfb7f5c1143b41e72e1f50f9f81a17df982
KS <kitsidhu@gmail.com>
2022-02-04 19:53:21 -0500

    set question font weight to bold when answer displayed & toggle off when answer hidden

--- 3-javascript/accordion/app.js
+++ 3-javascript/accordion/app.js
@@ -5,17 +5,23 @@ toggleAnswer();
 function toggleAnswer() {
   faqList.forEach((question) => {
     question.addEventListener('click', (event) => {
+      let question;
       let answer;
       if(event.target.classList.contains('question')) {
+        question = event.target.parentNode.querySelector('.question');
         answer = event.target.parentNode.querySelector('.answer');
       } else if(event.target.classList.contains('arrow-icon')) {
+        question = event.target.parentNode;
+        console.log(question);
         answer = event.target.parentNode.parentNode.querySelector('.answer');
       }
       if(answer.style.display === '') {
         answer.style.display = 'block';
+        question.style.fontWeight = 'bold';
       } else {
         answer.style.display = '';
+        question.style.fontWeight = 'normal';
       }
-    })
+    });
   });
 }
