cea5ce92cd6fa68117c34385da9a57bfda577c85
KS <kitsidhu@gmail.com>
2022-02-14 19:12:09 -0500

    fix styling in App.js

--- 4-react/social-media-site/src/App.js
+++ 4-react/social-media-site/src/App.js
@@ -3,57 +3,34 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 
 // import About from "./components/About";
-// import Container from "@mui/material/Container";
+import Container from "@mui/material/Container";
 // import Footer from "./components/Footer";
-// import Grid from "@mui/material/Grid";
+import Grid from "@mui/material/Grid";
 import Header from "./components/Header";
 import PostList from "./components/PostList";
 import Profile from "./components/Profile";
-
-// import { Switch } from "react-router-dom";
-
-// import { Routes } from "react-router-dom";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Header />
-//       {/* <Container maxWidth="xs" sx={{ marginY: 5 }}> */}
-//       {/* <Grid container justifyContent="center"> */}
-//       {/* <About /> */}
-//       {/* <Grid item xs={12} style={{ width: "100%" }}> */}
-//       <Switch>
-//         <Route exact path="/">
-//           <PostList />
-//         </Route>
-
-//         <Route exact path="/profile">
-//           <Profile />
-//         </Route>
-//       </Switch>
-//       {/* </Grid> */}
-//       {/* <Footer /> */}
-//       {/* // </Grid> */}
-//       {/* // </Container> */}
-//     </div>
-//   );
-// }
-
-// export default App;
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Switch>
-        <Route exact path="/">
-          <PostList />
-        </Route>
+      <Container maxWidth="xs" sx={{ marginY: 5 }}>
+        <Grid container justifyContent="center">
+          {/* <About /> */}
+          <Grid item xs={12} style={{ width: "100%" }}>
+            <Switch>
+              <Route exact path="/">
+                <PostList />
+              </Route>
 
-        <Route exact path="/profile">
-          <Profile />
-        </Route>
-      </Switch>
+              <Route exact path="/profile">
+                <Profile />
+              </Route>
+            </Switch>
+          </Grid>
+          {/* <Footer /> */}
+        </Grid>
+      </Container>
     </div>
   );
 }
