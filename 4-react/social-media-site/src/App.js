97b223a61d90053332dc59c79424ddaf1bc62d95
KS <kitsidhu@gmail.com>
2022-02-16 21:32:58 -0500

    fix prev project that I broke somehow

--- 4-react/social-media-site/src/App.js
+++ 4-react/social-media-site/src/App.js
@@ -1,26 +1,32 @@
 import "./App.css";
 
-import { BrowserRouter } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Header from "./components/Header";
-// import PostList from "./components/PostList";
-// import Profile from "./components/Profile";
-import Router from "./Router";
+import PostList from "./components/PostList";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Container maxWidth="xs" sx={{ marginY: 5 }}>
-          <Grid container justifyContent="center">
-            <Grid item xs={12} style={{ width: "100%" }}>
-              <Router />
-            </Grid>
+      <Header />
+      <Container maxWidth="xs" sx={{ marginY: 5 }}>
+        <Grid container justifyContent="center">
+          <Grid item xs={12} style={{ width: "100%" }}>
+            <Switch>
+              <Route exact path="/">
+                <PostList />
+              </Route>
+
+              <Route exact path="/profile">
+                <Profile />
+              </Route>
+            </Switch>
           </Grid>
-        </Container>
-      </BrowserRouter>
+        </Grid>
+      </Container>
     </div>
   );
 }
