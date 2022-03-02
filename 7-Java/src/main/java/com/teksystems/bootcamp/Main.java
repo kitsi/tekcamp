834c829495d68e5e80a96c55bb80d10e7f7dcec6
KS <kitsidhu@gmail.com>
2022-03-01 19:44:33 -0500

    return hashmap with chars & indexes to Main

--- 7-Java/src/main/java/com/teksystems/bootcamp/Main.java
+++ 7-Java/src/main/java/com/teksystems/bootcamp/Main.java
@@ -1,9 +1,9 @@
 package com.teksystems.bootcamp;
 
 import com.teksystems.bootcamp.additionalnputs.NumberAggregator;
+import com.teksystems.bootcamp.findCharacters.CharFinder;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
@@ -23,6 +23,8 @@ public class Main {
             userChoice = input.nextLine().toLowerCase();
             if((userChoice.equals("1")) || (userChoice.contains("number")) || (userChoice.contains("aggregator"))) {
                 runNumberAggregator();
+            } else if((userChoice.equals("2")) || (userChoice.contains("find")) || (userChoice.contains("characters"))) {
+                runFindCharacters();
             } else {
                 isQuit = quit();
             }
@@ -47,9 +49,9 @@ public class Main {
     private static void runNumberAggregator() {
         System.out.println();
         Scanner input = new Scanner(System.in);
-        int numberToAdd;
 
         boolean isQuitAggregator = false;
+        int numberToAdd;
         int total = 0;
 
         do {
@@ -62,11 +64,34 @@ public class Main {
             }
         } while(!isQuitAggregator);
         System.out.println("Your total is: " + total + "\n");
+
         try {
             TimeUnit.MILLISECONDS.sleep(1500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void runFindCharacters() {
+        System.out.println();
+        Scanner input = new Scanner(System.in);
+
+        boolean isQuitFinder = false;
+        String charactersToFind;
+        String stringToSearch;
+        Map<Character, List<Integer>> charPositions = new HashMap<>();
+
+        do {
+            System.out.println("Enter characters to find");
+            charactersToFind = input.nextLine();
+            System.out.println("Enter string to search");
+            stringToSearch = input.nextLine();
+            charPositions = CharFinder.findMatchPositions(charactersToFind, stringToSearch);
+            System.out.printf("%-12s | %-10s\n", "Character", "Positions");
+//            System.out.printf("-12s | %-10", "__________", "__________");
+//            for(HashMap<> posi)
+            System.out.println(charPositions);
+        } while(!isQuitFinder);
     }
 }
 
--- 7-Java/src/main/java/com/teksystems/bootcamp/findCharacters/CharFinder.java
+++ 7-Java/src/main/java/com/teksystems/bootcamp/findCharacters/CharFinder.java
@@ -1,11 +1,29 @@
-//package com.teksystems.bootcamp.findCharacters;
-//
-//import java.util.HashMap;
-//
-//public class CharFinder {
-//
-//    public static HashMap<Char, List<int>> findMatchPositions(String charsToFind, String stringToSearch) {
-//        //return a list of unique characters in first argument
-//        //return positions where those characters were found in second argument
-//    }
-//}
+package com.teksystems.bootcamp.findCharacters;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class CharFinder {
+
+    public static HashMap<Character, List<Integer>> findMatchPositions(String charsToFind, String stringToSearch) {
+
+        HashMap<Character, List<Integer>> charPositions = new HashMap<>();
+
+        char[] arrOfChars = charsToFind.toLowerCase().toCharArray();
+        for(char letter : arrOfChars) {
+            List<Integer> indexes = new ArrayList<Integer>();
+            int index = 0;
+
+            while(index != -1) {
+                index = stringToSearch.toLowerCase().indexOf(letter, index);
+                if(index != -1) {
+                    indexes.add(index);
+                    index++;
+                }
+            }
+            charPositions.put((Character) letter, indexes);
+        }
+        return charPositions;
+    }
+}
