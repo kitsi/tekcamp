c81d863b8a44f1493293e1130887f92d60f444e1
KS <kitsidhu@gmail.com>
2022-03-07 13:59:05 -0500

    set new target map with randomly generated values for each grid position

--- 7-Java/src/main/java/com/teksystems/bootcamp/gridHopper/BoardGenerator.java
+++ 7-Java/src/main/java/com/teksystems/bootcamp/gridHopper/BoardGenerator.java
@@ -1,12 +1,12 @@
 package com.teksystems.bootcamp.gridHopper;
 
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 public class BoardGenerator {
 
     public static LocationPointer[][] createBoard(int size, int seed) {
         LocationPointer[][] grid = new LocationPointer[size][size];
+        LocationPointer[][] targetMap = new LocationPointer[size][size];
         Random random = new Random();
         random.setSeed(seed);
 
@@ -14,11 +14,14 @@ public class BoardGenerator {
             for (int col = 0; col < grid[row].length; col++) {
 //                LocationPointer currentLocation = new LocationPointer(row, col);
                 LocationPointer targetLocation = new LocationPointer(random.nextInt(size), random.nextInt(size));
+//                System.out.print("(" + targetLocation.getTargetX() + ", " + targetLocation.getTargetY() + ") ");
+                targetMap[row][col] = new LocationPointer(targetLocation.getTargetX(), random.nextInt(size));
                 System.out.print("(" + targetLocation.getTargetX() + ", " + targetLocation.getTargetY() + ") ");
             }
             System.out.println();
         }
         System.out.println();
-        return grid;
+//        return grid;
+        return targetMap;
     }
 }
--- 7-Java/src/main/java/com/teksystems/bootcamp/gridHopper/BoardHopper.java
+++ 7-Java/src/main/java/com/teksystems/bootcamp/gridHopper/BoardHopper.java
@@ -3,7 +3,8 @@ package com.teksystems.bootcamp.gridHopper;
 public class BoardHopper {
 
     public static LocationPointer hop(LocationPointer[][] board) {
-        LocationPointer hopper = new LocationPointer(0, 0);
-        return hopper;
+
+        LocationPointer currentPosition = new LocationPointer(0, 0);
+        return currentPosition;
     }
 }
