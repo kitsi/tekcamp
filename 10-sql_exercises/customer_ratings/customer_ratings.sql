b18fd54112b4da5ea57b413a7633a57fd56a06b5
KS <kitsidhu@gmail.com>
2022-03-31 17:48:23 -0400

    WIP

---
+++ 10-sql_exercises/customer_ratings/customer_ratings.sql
@@ -0,0 +1,7 @@
+-- • Add an additional table to your schema, to store records of customer ratings for movies
+-- 	o Foreign key constraints/ cardinality should be set appropriately
+-- • Create (manually or randomly) 20-30 records in the customer ratings table
+-- • List each movie, including the highest, lowest, and average score they received, as well as the
+-- number of reviews
+
+USE sakila;
