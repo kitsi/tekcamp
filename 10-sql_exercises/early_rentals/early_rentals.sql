9f7d3b3fd5347c7ed95e8437f5fdc3b5a6cebb96
KS <kitsidhu@gmail.com>
2022-03-31 17:27:20 -0400

    add ex 7

--- 10-sql_exercises/early_rentals/early_rentals.sql
+++ 10-sql_exercises/early_rentals/early_rentals.sql
@@ -1,7 +1,11 @@
--- Retrieve a list of the most recent 10 rentals that occurred before 10AM
--- o Include all columns from rental, as well as the date (not including the time) that the rental
--- 	 occurred
+-- Retrieve a list of the most recent
+-- 10 rentals 
+-- that occurred before 10AM
+-- Include all columns from rental, 
+-- as well as the date (not including the time) that the rental occurred
 
 USE sakila;
-select * from rental
-WHERE HOUR(rental_date)<'10';
+SELECT rental_id, date(rental_date) AS rent_date, inventory_id, customer_id, return_date, staff_id, last_update from rental
+WHERE HOUR(rental_date)<'10'
+ORDER BY rental_date DESC
+LIMIT 10;
