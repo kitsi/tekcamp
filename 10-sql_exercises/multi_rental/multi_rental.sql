d295874ce017fcb95c5cd1868538bca6924886a4
KS <kitsidhu@gmail.com>
2022-04-01 17:21:42 -0400

    add completed ex 10 files

--- 10-sql_exercises/multi_rental/multi_rental.sql
+++ 10-sql_exercises/multi_rental/multi_rental.sql
@@ -4,5 +4,13 @@
 -- 		and list the
 -- 			customer, rental date, number of rentals, and total amount paid for that day’s rentals
 
-use sakila;
+USE sakila;
 
+SELECT (CONCAT(customer.first_name, ' ', customer.last_name)) customer, 
+(DATE(rental.rental_date)) rental_date, 
+(COUNT(rental.rental_id)) num_rentals, 
+(SUM(payment.amount)) days_total
+FROM customer
+JOIN rental ON rental.customer_id = customer.customer_id
+JOIN payment ON payment.rental_id = rental.rental_id
+GROUP BY customer.customer_id, DATE(rental.rental_date);
