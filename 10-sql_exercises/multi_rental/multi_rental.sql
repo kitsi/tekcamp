771738e2398a64967ceb2f97a2072ed014c320af
KS <kitsidhu@gmail.com>
2022-04-01 19:01:53 -0400

    setup all joins

--- 10-sql_exercises/multi_rental/multi_rental.sql
+++ 10-sql_exercises/multi_rental/multi_rental.sql
@@ -7,10 +7,11 @@
 USE sakila;
 
 SELECT (CONCAT(customer.first_name, ' ', customer.last_name)) customer, 
-(DATE(rental.rental_date)) rental_date, 
-(COUNT(rental.rental_id)) num_rentals, 
-(SUM(payment.amount)) days_total
+	(DATE(rental.rental_date)) rental_date, 
+	(COUNT(rental.rental_id)) num_rentals, 
+	(SUM(payment.amount)) days_total
 FROM customer
-JOIN rental ON rental.customer_id = customer.customer_id
-JOIN payment ON payment.rental_id = rental.rental_id
-GROUP BY customer.customer_id, DATE(rental.rental_date);
+	JOIN rental ON rental.customer_id = customer.customer_id
+	JOIN payment ON payment.rental_id = rental.rental_id
+GROUP BY customer.customer_id, DATE(rental.rental_date)
+HAVING (COUNT(rental.rental_id)) > 1;
