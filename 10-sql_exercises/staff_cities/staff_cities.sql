b3340d8bb717963387b1f2af5c1b6d80f1a26e2d
KS <kitsidhu@gmail.com>
2022-04-01 16:54:04 -0400

    upcase keywords in staff_cities sql

--- 10-sql_exercises/staff_cities/staff_cities.sql
+++ 10-sql_exercises/staff_cities/staff_cities.sql
@@ -1,12 +1,12 @@
 -- Retrieve a list of all staff that have their home store in a certain city (a city of your choice)
 -- city of choice = Woodridge (Australia)
 
-use sakila;
-select staff.first_name, 
+USE sakila;
+SELECT staff.first_name, 
 staff.last_name, 
-city.city as home_store_city
-from staff
-join store on staff.store_id = store.store_id
-join address on store.address_id = address.address_id
-join city on address.city_id = city.city_id
-where city.city_id=576;
+city.city AS home_store_city
+FROM staff
+JOIN store ON staff.store_id = store.store_id
+JOIN address ON store.address_id = address.address_id
+JOIN city ON address.city_id = city.city_id
+WHERE city.city_id=576;
