41f13641b6a94580ec1e076af9dbd39ed5917fca
KS <kitsidhu@gmail.com>
2022-04-01 21:30:13 -0400

    add customer name filter by letter 'k'

--- 10-sql_exercises/customer_favorites/customer_favorites.sql
+++ 10-sql_exercises/customer_favorites/customer_favorites.sql
@@ -7,15 +7,18 @@ use sakila;
 WITH customer_rentals
 AS (SELECT 
 		ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY customer_id, name) rownum1,
-		customer_id,
+		rental.customer_id,
+        (CONCAT(customer.first_name, ' ', customer.last_name)) customer_name,
 		category.name
 	FROM rental
 	JOIN inventory ON inventory.inventory_id = rental.inventory_id
 	JOIN film ON film.film_id = inventory.film_id
 	JOIN film_category ON film_category.film_id = film.film_id
 	JOIN category ON category.category_id = film_category.category_id
+    JOIN customer ON rental.customer_id = customer.customer_id
 	GROUP BY customer_id, name, rental_date
-	ORDER BY customer_id, rental_date)
+    HAVING customer_name like '%k%'
+    ORDER BY customer_id, rental_date)
 
 SELECT * FROM (
 	SELECT * ,
@@ -25,6 +28,7 @@ SELECT * FROM (
 					ROW_NUMBER() OVER (PARTITION BY customer_id, customer_rentals.name ORDER BY customer_id) rownum2,
 					customer_rentals.rownum1,
 					customer_id,
+                    customer_rentals.customer_name,
 					(customer_rentals.name) favorite_category
 				FROM customer_rentals
 				WHERE
